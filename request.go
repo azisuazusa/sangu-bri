2ddae4e281c6a1beb2a01a61035e0c3048ebe7da
abhisuta <abhiyasasb@gmail.com>
2019-09-19 12:48:26 +0700

    - add get report - change several variable names on request and response to English for consistency

--- request.go
+++ request.go
@@ -4,8 +4,15 @@ type CreateVaRequest struct {
 	InstitutionCode string `json:"institutionCode"`
 	BrivaNo         string `json:"brivaNo"`
 	CustCode        string `json:"custCode"`
-	Nama            string `json:"nama"`
+	Name            string `json:"nama"`
 	Amount          string `json:"amount"`
-	Keterangan      string `json:"keterangan"`
+	Description     string `json:"keterangan"`
 	ExpiredDate     string `json:"expiredDate"`
+}
+
+type GetReportVaRequest struct {
+	InstitutionCode string
+	BrivaNo         string
+	StartDate       string
+	EndDate         string
 }
--- responses.go
+++ responses.go
@@ -18,8 +18,27 @@ type VaData struct {
 	InstitutionCode string `json:"institutionCode"`
 	BrivaNo         string `json:"brivaNo"`
 	CustCode        string `json:"custCode"`
-	Nama            string `json:"nama"`
+	Name            string `json:"nama"`
 	Amount          string `json:"amount"`
-	Keterangan      string `json:"keterangan"`
+	Description     string `json:"keterangan"`
 	ExpiredDate     string `json:"expiredDate"`
+}
+
+type VaReportResponse struct {
+	Status              bool           `json:"status"`
+	ResponseCode        string         `json:"responseCode"`
+	ResponseDescription string         `json:"responseDescription"`
+	ErrDesc             string         `json:"errDesc"`
+	Data                []VaReportData `json:"data"`
+}
+
+type VaReportData struct {
+	BrivaNo     string `json:"brivaNo"`
+	CustCode    string `json:"custCode"`
+	Nama        string `json:"nama"`
+	Amount      string `json:"amount"`
+	Description string `json:"keterangan"`
+	PaymentDate string `json:"paymentDate"`
+	TellerId    string `json:"tellerid"`
+	AccountNo   string `json:"no_rek"`
 }
